42b5067405d9d3d3f496ae6b98439af3bed7c99a
Cesar Carlos <cesar.g.carlos@gmail.com>
2012-11-21 00:52:37 -0800

    [Add] Class FunLearningFactor
    * Compute flf matrix of tutor-player pairs.

--- flf.py
+++ flf.py
@@ -1,8 +1,6 @@
 from sys import argv, exit, stderr
 
-
-VOWELS = "aeiouy"
-CONSONANTS = "bcdfghjklmnpqrstvwxz"
+from flf_process import FunLearningFactor
 
 
 def main(argv):
@@ -14,31 +12,9 @@ def main(argv):
     tutors_file = argv[0]
     players_file = argv[1]
 
-    # Populate tutor dict.
-    t_dict = {}
-    with open(tutors_file, 'r') as tf:
-        for i, tutor in enumerate(tf.readlines()):
-            tutor = tutor.strip()
-            t_dict[i] = {'name': tutor, 'len': len(tutor),
-                         'odd': True if len(tutor) % 2 != 0 else False}
-
-    # Populate player dict.
-    p_dict = {}
-    with open(players_file, 'r') as pf:
-        for i, player in enumerate(pf.readlines()):
-            player = player.strip()
-            vowels = sum(player.lower().count(ch) for ch in VOWELS)
-            consonants = sum(player.lower().count(ch) for ch in CONSONANTS)
-            p_dict[i] = {'name': player, 'len': len(player),
-                         'num_vowels': vowels, 'num_consonants': consonants}
-
-    # Do we have an equal number of tutors and players?
-    t_len = len(t_dict)
-    p_len = len(p_dict)
-    if t_len != p_len:
-        print >> stderr, "There is not and equal number of tutors and players."
-        print >> stderr, "Tutors: %d, Players: %d" % (t_len, p_len)
-        exit(2)  # TODO: look up proper exit code
+    flf = FunLearningFactor(tutors_file, players_file)
+    flf.fill_flf_matrix()
+    print flf.print_flf_matrix()
 
 
 if __name__ == '__main__':
---
+++ flf_process.py
@@ -0,0 +1,75 @@
+from fractions import gcd
+from sys import exit, stderr
+
+VOWELS = "aeiouy"
+CONSONANTS = "bcdfghjklmnpqrstvwxz"
+
+
+class FunLearningFactor(object):
+    def __init__(self, tutors_file, players_file):
+        self.flf_matrix = []
+
+        # Populate tutor dict.
+        self.t_dict = {}
+        with open(tutors_file, 'r') as tf:
+            for i, tutor in enumerate(tf.readlines()):
+                tutor = tutor.strip()
+                self.t_dict[i] = {'name': tutor, 'len': len(tutor),
+                    'odd': True if len(tutor) % 2 != 0 else False}
+
+        # Populate player dict.
+        self.p_dict = {}
+        with open(players_file, 'r') as pf:
+            for i, player in enumerate(pf.readlines()):
+                player = player.strip()
+                vowels = sum(player.lower().count(ch) for ch in VOWELS)
+                consonants = sum(player.lower().count(ch) for ch in CONSONANTS)
+                self.p_dict[i] = {'name': player, 'len': len(player),
+                                  'num_vowels': vowels,
+                                  'num_consonants': consonants}
+
+        # Do we have an equal number of tutors and players?
+        t_len = len(self.t_dict)
+        p_len = len(self.p_dict)
+        if t_len != p_len:
+            print >> stderr, "Not an equal number of tutors and players."
+            print >> stderr, "Tutors: %d, Players: %d" % (t_len, p_len)
+            exit(1)
+
+    def fill_flf_matrix(self):
+        for t_itor in xrange(len(self.t_dict)):
+            matrix_row = []
+            for p_itor in xrange(len(self.p_dict)):
+                flf = self.calc_flf(self.t_dict[t_itor], self.p_dict[p_itor])
+                matrix_row.append(flf)
+            self.flf_matrix.append(matrix_row)  # add new row
+
+    def calc_flf(self, tutor, player):
+        """Calculates flf of a tutor-player pair."""
+
+        flf = None
+        if tutor['odd']:
+            flf = player['num_vowels'] * 1.5
+        else:
+            flf = player['num_consonants'] * 1.0
+
+        # Check for common factors other than 1.
+        if gcd(tutor['len'], player['len']) > 1:
+            flf = flf + (flf * 0.5)
+
+        return flf
+
+    def print_flf_matrix(self):
+        """Return a string representation of the flf_matrix."""
+        matrix_str = '\n'
+        matrix_dim = len(self.t_dict)  # Both tutor and player size is equal.
+        for row in xrange(matrix_dim):
+            matrix_str += '|'
+            for col in xrange(matrix_dim):
+                matrix_str += str(self.flf_matrix[row][col]) + '|'
+            matrix_str += '\n'
+
+        # Print out the vertical line for board.
+        matrix_str += matrix_dim * '--' + '-\n'
+
+        return matrix_str
